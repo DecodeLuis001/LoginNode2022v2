5b6754d0108cce9529b8065102ec478a0eb54e30
Luis Arroyo Nuñez <luis_arz@MacBook-Air-de-Luis.local>
2022-04-22 18:51:47 -0500

    subir app a heorku usando localhost:3001

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyparser = require('body-parser')
+const cors =require('cors')
 
+//Configuracion de rotas
+const authRoutes = require('./routes/auth')             //import routes.
 const dashboardRoutes = require('./routes/dashboard')
 const verifyToken = require('./routes/validate-token')
-
-require('dotenv').config()
-
-const app = express()
-
-const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.c9it5.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
 const options =     {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }
+const corsOptions = {
+    origin: "*",
+    optionsSuccessStatus: 200
+}
+
+const app = express()
+const uri = 'mongodb://localhost:27017/appLenguajes'
+//const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.c9it5.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
+//iniciar el servidor
+const PORT = process.env.port || 3001
+
+app.listen(PORT, () => {
+    console.log(`Servidor Trabajando: ${PORT}`) //las `` sirven para mezclar con variables los string.
+}) 
+
+require('dotenv').config()
+
 mongoose.connect(uri, options)
     .then(() => console.log('Conectado a la BD.'))
     .catch((e) => console.log('error ' + e)
@@ -21,10 +35,10 @@ mongoose.connect(uri, options)
 
 //Forma de capturar el body en las petciones.
 app.use(bodyparser.urlencoded({extended: false}))
-app.use(bodyparser.json())
-
+app.use(bodyparser.json())              //Mandamos la info en formato jason.
+app.use(cors(corsOptions))
+app.get('/api/user', authRoutes)        //para registrar el usuario.
 app.use('/api/dashboard', verifyToken, dashboardRoutes)
-
 // route del middleware.
 app.get('/', (req, res) => {
     res.json({
@@ -32,13 +46,3 @@ app.get('/', (req, res) => {
         mensaje: 'Works!'
     })
 })
-
-const authRoutes = require('./routes/auth') 
-app.use('/api/user', authRoutes)        //para registrar el usuario.
-
-
-const PORT = process.env.port || 3001
-
-app.listen(PORT, () => {
-    console.log(`Servidor Trabajando: ${PORT}`) //las `` sirven para mezclar con variables los string.
-}) 
